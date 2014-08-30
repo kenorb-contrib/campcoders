90d63761eaf3af79de7cd75756b234b3c90dd575
Thanchet <phamvanhieu861992@gmail.com>
2014-08-30 09:43:30 +0700

    update javascript

--- wp-content/themes/asteria-lite/header.php
+++ wp-content/themes/asteria-lite/header.php
@@ -3,11 +3,53 @@
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head>
 <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <script>
 	$(window).load(function(){
 	    $('#cntctfrm_contact_form').attr('action','http://theme.campcoders.com/job-search/#cntctfrm_thanks');
+	    $('.slider-wrapper').css('height',$(window).height()-85);
+	    var current_cate  = <?php echo json_encode(get_the_category()[0]->slug); ?>;
+	    if(current_cate == 'portfolio'){
+	    	gtTop('nav_menu-4');
+	    }
+	    if(current_cate == 'blog'){
+	    	gtTop('nav_menu-10');
+	    }
+
+	    function gtTop(id){
+	    	var e = $('#'+id);
+	    	e.addClass('notCl');
+	    	$(e).prependTo('#sidebar .widgets');
+	    	e.find('.menu').addClass('collapse');
+	    	e.find('.menu').collapse('toggle');
+	    	e.find('.widgettitle').attr('onclick',"showCollapse('"+e.attr('id')+"')");
+	    	var notClTitle = e.find('.widgettitle').text();
+	    		notClTitle += '<span class="caret" style="margin-left: 10px"></span>';
+	    	e.find('.widgettitle').html(notClTitle);
+	    	var collapse = $('#sidebar .widgets > div:not(.notCl)');
+	    		collapse.addClass('cl');
+	    		collapse.find('.menu').addClass('collapse');
+	    		collapse.find('.widgettitle').each(function(){
+	    			$(this).attr('onclick',"showCollapse('"+$(this).parent().parent().attr('id')+"')");
+	    			var title = $(this).text();
+	    			title += '<span class="caret" style="margin-left: 10px"></span>';
+	    			$(this).html(title);
+	    		})
+	    }
+
 	})
+	function showCollapse(id){
+	    $('#'+id).find('.menu').collapse('toggle');
+	}
 </script>
+
 </script>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=UTF-8" />	
 <title><?php wp_title( '|', true, 'right' ); ?></title>
